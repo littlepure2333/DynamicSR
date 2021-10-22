0fc5882f0cea1dea226fe59a756ef3a51f035098
littlepure2333 <rz8217525@163.com>
2021-10-22 05:12:46 -0400

    update edsr_dynamic

--- model/edsr_dynamic.py
+++ model/edsr_dynamic.py
@@ -3,15 +3,6 @@ from model import common
 
 import torch.nn as nn
 
-url = {
-    'r16f64x2': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x2-1bc95232.pt',
-    'r16f64x3': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x3-abf2a44e.pt',
-    'r16f64x4': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x4-6b446fab.pt',
-    'r32f256x2': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_x2-0edfb8a3.pt',
-    'r32f256x3': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_x3-ea3ef2c6.pt',
-    'r32f256x4': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_x4-4f62e9ef.pt'
-}
-
 def make_model(args, parent=False):
     return EDSR(args)
 
@@ -25,26 +16,14 @@ class EDSR(nn.Module):
         kernel_size = 3 
         scale = args.scale[0]
         act = nn.ReLU(True)
-        url_name = 'r{}f{}x{}'.format(n_resblocks, n_feats, scale)
-        if url_name in url:
-            self.url = url[url_name]
-        else:
-            self.url = None
+
+        self.exit_interval = args.exit_interval
+
         self.sub_mean = common.MeanShift(args.rgb_range)
         self.add_mean = common.MeanShift(args.rgb_range, sign=1)
 
         # define head module
         m_head = [conv(args.n_colors, n_feats, kernel_size)]
-
-        # define body module
-        # m_body = []
-        # for i in range(n_resblocks // 4):
-        #     res_unit = [
-        #         common.ResBlock(
-        #         conv, n_feats, kernel_size, act=act, res_scale=args.res_scale
-        #     ) for _ in range(4)
-        #     ]
-        #     m_body.append(nn.Sequential(*res_unit))
         
         m_body = [
             common.ResBlock(
@@ -61,7 +40,7 @@ class EDSR(nn.Module):
         ]
 
         self.head = nn.Sequential(*m_head)
-        self.body = nn.ModuleList(m_body)
+        self.body = nn.Sequential(*m_body)
         self.tail = nn.Sequential(*m_tail)
 
     def forward(self, x):
@@ -72,9 +51,8 @@ class EDSR(nn.Module):
         output = []
         for i, layer in enumerate(self.body):
             res = layer(res)
-            if i % 4 == 3:
-                y = x + res
-                output.append(self.add_mean(self.tail(y)))
+            if i % self.exit_interval == (self.exit_interval-1):
+                output.append(self.add_mean(self.tail(x + res)))
 
         # x = self.tail(res)
         # x = self.add_mean(x)
